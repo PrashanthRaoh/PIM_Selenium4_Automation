500d1e04ac12ffb52c530a0fc7073d41e4756902
Prashanth Rao <raoad@timken.com>
2024-11-25 11:30:21 +0530

    Updated Login page

--- PIM_Selenium4_Automation/src/main/java/pages/Login_Page.java
+++ PIM_Selenium4_Automation/src/main/java/pages/Login_Page.java
@@ -1,5 +1,6 @@
 package pages;
 
+import java.io.IOException;
 import java.time.Duration;
 
 
@@ -9,10 +10,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Login_Page {
+import common_functions.Utils;
+
+public class Login_Page  {
 	
 	private WebDriver driver;
     private WebDriverWait wait;
+    public Utils utils;
+    public HomePage homePage;
     
     private By emailField = By.cssSelector("[type='email']");
     private By submitButton = By.cssSelector("[type='submit']");
@@ -20,15 +25,30 @@ public class Login_Page {
     public Login_Page(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(50));
+        homePage = new HomePage(driver);
+        utils = new Utils(driver);
     }
 
     public void enterEmail(String email) {
         WebElement emailElement = wait.until(ExpectedConditions.elementToBeClickable(emailField));
         emailElement.sendKeys(email);
     }
-
+	
+	public void LaunchSite() {
+		driver.get("https://timkenfs.riversand.com/home");
+	}
+	
     public void clickSubmit() {
         driver.findElement(submitButton).click();
     }
-
+	
+	public void LogintoPIM() throws IOException {
+		LaunchSite();
+		enterEmail("raoad@timken.com");
+		clickSubmit();
+		utils.waitForElement(homePage.HomePage_SearchButton(), "clickable");
+		utils.Takescreenshot();
+	}
+	
+	
 }
